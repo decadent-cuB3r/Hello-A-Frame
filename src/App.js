d9ce8887d7092b209c4930c768ad553c81f1cf06
阿敗塗一地 <71422011+decadent-cuB3r@users.noreply.github.com>
2022-04-15 17:25:58 +0800

    put vr-image in Sky

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import 'aframe';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  Scene,
+  Box,
+  Sphere,
+  Cylinder,
+  Plane,
+  Sky,
+} from '@belivvr/aframe-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Scene>
+      <Sky src='assets/images/vr.jpg'>
+      </Sky>
+    </Scene>
   );
 }
 
